0a5f8de20261b3c72a6baf6e432a35d76ab558f4
luch <luchwork@mail.ru>
2019-05-26 11:58:02 +0300

    Implemented a simple console app that matches an input image to an image in the database

---
+++ src/demo_app/match.py
@@ -0,0 +1,107 @@
+"""Uses feature_vectors.txt file produced by process_database.py to match an input image with an image in the
+   database"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import sys
+import os
+import argparse
+import facenet
+import json
+import numpy as np
+from PIL import Image
+from scipy import misc
+
+
+def main(args):
+    with tf.Graph().as_default():
+
+        with tf.Session() as sess:
+
+            # Load the model
+            facenet.load_model(args.model)
+
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("image_batch_p:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+
+            # Load and preprocess image
+            original_img = misc.imread(args.image_path, mode='RGB')
+            img = misc.imresize(original_img, (args.image_size, args.image_size), interp='bilinear')
+            img = np.expand_dims(img, axis=0)
+            img = (img - img.min()) / (img.max() - img.min())
+            img = img * 2 - 1
+
+            # Calculate target image embedding
+            target_emb = sess.run(embeddings, feed_dict={images_placeholder: img, phase_train_placeholder: False})
+
+            dist_list = []
+            paths_list = []
+
+            with open("feature_vectors.txt", "r") as file:
+
+                for line in file:
+
+                    # Calculate embedding for each image in the database
+                    line = line.strip()
+                    emb_dict = json.loads(line)
+                    path, emb = list(emb_dict.items())[0]
+                    emb = np.array(emb)
+
+                    # Then calculate distance to the target
+                    dist = np.sqrt(np.sum(np.square(np.subtract(emb, target_emb[0]))))
+                    dist_list.append(dist)
+                    paths_list.append(path)
+
+            ind = np.argmin(dist_list)
+            print(dist_list[ind], paths_list[ind])
+
+            closest_img = misc.imread(paths_list[ind], mode='RGB')
+            show_result(original_img, closest_img)
+
+
+def show_result(original_img, closest_img):
+    merged_img = resize_and_merge(original_img, closest_img)
+    output = Image.fromarray(merged_img)
+    output.show()
+
+
+def resize_and_merge(img_a, img_b):
+    """Merges 2 images. Images should be passed as arrays"""
+    shape_a, shape_b = img_a.shape, img_b.shape
+    new_width = min(shape_a[0], shape_b[0], 1024)
+    new_height = min(shape_a[1], shape_b[1], 512)
+
+    resized_img_a = misc.imresize(img_a, (new_width, new_height), interp='bilinear')
+    resized_img_b = misc.imresize(img_b, (new_width, new_height), interp='bilinear')
+
+    assert resized_img_a.shape == resized_img_b.shape
+
+    merged_img = np.concatenate((resized_img_a, resized_img_b), axis=1)
+
+    return merged_img
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('image_path', type=str, help='Image to compare')
+    parser.add_argument('--data_root', type=str,
+                        help='Path to data directory which needs to forward passed through the network',
+                        default='../datasets/weather-localization-training-set/')
+    parser.add_argument('--model', type=str,
+                        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf '
+                             '(.pb) file',
+                        default='../models/20190501-055657-facenet+gan+smaug')
+    parser.add_argument('--image_size', type=int,
+                        help='Image size (height, width) in pixels.',
+                        default=256)
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
---
+++ src/demo_app/process_database.py
@@ -0,0 +1,71 @@
+"""Uses trained model to calculate embeddings and saves them in a feature_vectors.txt file"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import sys
+import os
+import argparse
+import facenet
+import json
+
+from data import ImageData
+
+
+def main(args):
+    with tf.Graph().as_default():
+
+        with tf.Session() as sess:
+
+            # Load the model
+            facenet.load_model(args.model)
+
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("image_batch_p:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+
+            # Load data
+            dataset = facenet.get_dataset(args.data_root)
+            image_paths, _ = facenet.get_image_paths_and_labels(dataset)
+            images = ImageData(image_paths, sess, load_size=args.image_size, batch_size=1)
+            nrof_images = len(images)
+
+            with open("feature_vectors.txt", "w") as file:
+
+                for i in range(nrof_images):
+                    img = images.batch()
+                    path = os.path.abspath(image_paths[i])
+
+                    # Run forward pass to calculate embeddings
+                    feed_dict = {images_placeholder: img, phase_train_placeholder: False}
+                    emb = sess.run(embeddings, feed_dict=feed_dict)
+
+                    line_dict = {path: emb[0].tolist()}
+                    json_data = json.dumps(line_dict)
+
+                    file.write(json_data)
+                    file.write('\n')
+                    print(path)
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data_root', type=str,
+                        help='Path to data directory which needs to forward passed through the network',
+                        default='../datasets/weather-localization-training-set/')
+    parser.add_argument('--model', type=str,
+                        help='Could be either a directory containing the meta_file and ckpt_file or a model protobuf '
+                             '(.pb) file',
+                        default='../models/20190501-055657-facenet+gan+smaug')
+    parser.add_argument('--image_size', type=int,
+                        help='Image size (height, width) in pixels.',
+                        default=256)
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
